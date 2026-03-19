dca09589ab349e3d34061a8a269c2678d6913e2d
dinosfeu4 <wwroc444@gmail.com>
2026-03-19 18:51:18 -0400

    Update echo_defines.lua

--- common/defines/echo_defines.lua
+++ common/defines/echo_defines.lua
@@ -204,5 +204,5 @@ NDefines.NGovernment.GOVERNMENT_REFORM_COST_CENTRALIZE_STATE = 0
 NDefines.NGovernment.CENTRALIZE_STATE_YEARS = 5
 NDefines.NGame.DAYS_BEHIND_PAUSE = 90					-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will pause
 NDefines.NGame.DAYS_BEHIND_LOWER_SPEED = 90			-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will slow down
-NDefines.NMilitary.NOMAD_PLAINS_SHOCK_BONUS = 0.15,                 --
-NDefines.NMilitary.NOMAD_NON_PLAINS_SHOCK_PENALTY = -0.15,
+NDefines.NMilitary.NOMAD_PLAINS_SHOCK_BONUS = 0.15                --
+NDefines.NMilitary.NOMAD_NON_PLAINS_SHOCK_PENALTY = -0.15
