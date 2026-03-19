4eb9019e430b1ece18dba088704dacc5e526651b
dinosfeu4 <wwroc444@gmail.com>
2026-03-19 18:45:08 -0400

    horde

--- common/defines/echo_defines.lua
+++ common/defines/echo_defines.lua
@@ -202,6 +202,7 @@ NDefines.NGovernment.LEADER_AS_MONARCH_TRADITION = 15
 NDefines.NGovernment.CENTRALIZE_STATE_ADM_COST = 150
 NDefines.NGovernment.GOVERNMENT_REFORM_COST_CENTRALIZE_STATE = 0
 NDefines.NGovernment.CENTRALIZE_STATE_YEARS = 5
-
 NDefines.NGame.DAYS_BEHIND_PAUSE = 90					-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will pause
 NDefines.NGame.DAYS_BEHIND_LOWER_SPEED = 90			-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will slow down
+NDefines.NMilitary.NOMAD_PLAINS_SHOCK_BONUS = 0.15,                 --
+NDefines.NMilitary.NOMAD_NON_PLAINS_SHOCK_PENALTY = -0.15,
